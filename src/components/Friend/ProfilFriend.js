490811dced2b120c8cfe77f39be4ba9e01090525
Votre Nom <Vous@exemple.com>
2021-05-19 11:38:48 +0200

    modify css

--- src/components/Friend/ProfilFriend.js
+++ src/components/Friend/ProfilFriend.js
@@ -53,21 +53,14 @@ const ProfilFriend = ( { uidFriend } ) => {
         dispatch(unblockUser(uidFriend, uid));
     }
 
-    const handleCreate = async (e) => {
+    const handleCreate = e => {
         e.preventDefault();
         createRoomAndNotif();
-        roomAsync()
+        dispatch(getRoomChat(uid, uidFriend, count))
         document.getElementById('chat-user').scrollIntoView();
     }
     
     
-    const roomAsync = async () => {
-        if(uid && uidFriend){
-            dispatch(getUserFriend(uidFriend));
-            await dispatch(getRoomChat(uid, uidFriend, count));
-            SetLoadData(true);
-        } 
-    }
     
     useEffect(() => {        
         if(uidFriend) dispatch(getPostUser(uidFriend));
@@ -77,13 +70,14 @@ const ProfilFriend = ( { uidFriend } ) => {
             setLoadMessages(false)
             
         }
+        const roomAsync = async () => {
+            if(uid && uidFriend){
+                dispatch(getUserFriend(uidFriend));
+                await dispatch(getRoomChat(uid, uidFriend, count));
+                SetLoadData(true);
+            } 
+        }
         roomAsync()
-           
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [loadMessages, uid, loadData, dispatch, uidFriend, count, blocked])
-    
-    
-    useEffect(() => {
         if(!isEmpty(roomChat)){
             const notifs = roomChat.data[0].notifications;
             const arrNotifs = notifs.map(notif => {
@@ -94,10 +88,13 @@ const ProfilFriend = ( { uidFriend } ) => {
             if(!isEmpty(arrNotifs) && roomChat.data[0]._id) {
                 dispatch(deleteNotifs(roomChat.data[0]._id, uidFriend))
                 if(uid) dispatch(getNotifs(uid))
-            }
+            } 
         }
+           
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[handleCreate, dispatch, uid, uidFriend])
+    }, [loadMessages, uid, loadData, dispatch, uidFriend, count, blocked]) 
+
+    console.log(roomChat);
     
     return (
         <>
@@ -128,15 +125,15 @@ const ProfilFriend = ( { uidFriend } ) => {
                             <h3>Photo de profil</h3>
                             <img src={userDataFriend.picture} alt='picUserFriend'/>
                             <h1><FollowHandler idToFollow={userDataFriend._id} type={'suggestion'} /></h1>
-                            <div className='button-chat' >
-                                {blocked && (
-                                    <button onClick={handleUnblocked} >Débloqué</button>
-                                )}
-                                {!blocked && (
-                                    <button onClick={handleBlocked} >Bloqué</button>
-                                )}
-                                {isEmpty(roomChat) &&  <button onClick={handleCreate}>Envoyer message</button> }                   
-                            </div>
+                            {blocked && (
+                                <button onClick={handleUnblocked} >Débloqué</button>
+                            )}
+                            {!blocked && (
+                                <button onClick={handleBlocked} >Bloqué</button>
+                            )}
+                            {isEmpty(roomChat) &&  <form action='' onSubmit={handleCreate}>
+                                        <input type='submit' value='Envoyer message' />
+                                        </form>}                        
                         </div>
                         <div className='right-part' >
                             <div className='bio-update' >
